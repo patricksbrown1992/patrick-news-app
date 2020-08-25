35dae535943fb17c57b8ab519644ac60236d794f
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-08-25 18:38:26 -0400

    better stylization

--- src/App.js
+++ src/App.js
@@ -13,10 +13,7 @@ class App extends React.Component {
   
   }
 
-  componentDidMount(){
-    this.getArticles().then(articles => (this.receiveArticles(articles)))
-    // this.setState({loaded: true})
-  }
+
 
   handleChange() {
 
@@ -31,10 +28,22 @@ class App extends React.Component {
   }
 
   receiveArticles(articles){
+    let arr = [];
+    let i = 0;
+    
+    while(i < articles.articles.length-1){
+      let first_article = articles.articles[i];
+      let second_article = articles.articles[i+1];
+      
+      let div_1 = <div id={i} className='article-div'><div className = 'article-div-upper' style={{ 'backgroundImage': `url(${first_article.urlToImage})`}}></div><div className = 'article-div-lower'>{first_article.title}{first_article.description}<a href="google.com">Read More</a></div></div>
+      let div_2 = <div id={i} className='article-div'><div className = 'article-div-upper' style={{ 'backgroundImage': `url(${second_article.urlToImage})`}}></div><div className = 'article-div-lower'>{second_article.title}{second_article.description}<a href="google.com">Read More</a></div></div>
+      let combined_div = <div className='combined-div'>{div_1}{div_2}</div>
+      arr.push(combined_div)
+      i += 2;
 
-    let arr = articles.articles.map(article => {
-      return <li style={{ 'backgroundImage': `url(${article.urlToImage})`}}>{article.title}</li>
-    })
+    }
+
+
     this.setState({articles: arr})
   
   }
@@ -49,9 +58,7 @@ class App extends React.Component {
     } else {
       return $.ajax({
         
-        url: 'http://newsapi.org/v2/top-headlines?' +
-        'country=us&' +
-        'apiKey=e5040c51bda24558962c4af06ff1ee2b'
+        url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=e5040c51bda24558962c4af06ff1ee2b'
       })
     }
 
@@ -64,16 +71,17 @@ class App extends React.Component {
 
   render(){
 
-    if(!this.state.articles.length){
-      return null;
-    }
    
     return (
       <div className="App">
-        <h1>News App</h1>
-        <input type="text" value={this.state.seach} placeholder='search' onChange={this.handleChange()} />
-        <button onClick={this.fetchArticles}>submit</button>
-        {this.state.articles}
+        <h1>Patrick News App</h1>
+        <div>
+          <input type="text" value={this.state.seach} placeholder='search' onChange={this.handleChange()} />
+          <button onClick={this.fetchArticles}>submit</button>
+        </div>
+        
+        <div className='articles-div'>{this.state.articles}</div>
+        
       </div>
     );
   }
