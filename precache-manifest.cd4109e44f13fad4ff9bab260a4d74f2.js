be210a61744ed56ac5c1780e890dbe95db7e2360
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-08-28 20:45:26 -0400

    Updates

---
+++ precache-manifest.cd4109e44f13fad4ff9bab260a4d74f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1572a84e0a87e883f4fde31b58c06c26",
+    "url": "/patrick-news-app/index.html"
+  },
+  {
+    "revision": "9b93d32a339d42faede1",
+    "url": "/patrick-news-app/static/css/main.4e49e6f7.chunk.css"
+  },
+  {
+    "revision": "caf820ca1b7608cae738",
+    "url": "/patrick-news-app/static/js/2.5d29db61.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/patrick-news-app/static/js/2.5d29db61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b93d32a339d42faede1",
+    "url": "/patrick-news-app/static/js/main.53c58ec1.chunk.js"
+  },
+  {
+    "revision": "441c5bbe47a90d0e7bd5",
+    "url": "/patrick-news-app/static/js/runtime-main.1e14bfb8.js"
+  }
+]);
